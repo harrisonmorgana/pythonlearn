cf0715bad84cc44e070be35fcac9dc386f0c89ac
harrison <harrison.morgana@outlook.com>
2025-07-29 14:28:57 +0100

    2nd commit python added

---
+++ Chatbot.py
@@ -0,0 +1,12 @@
+print("Hello My name Chester")
+print("I am a chatbot")
+
+print("I can help you with your queries")
+
+print("What is your name?")
+input_name = input("Please enter your name: ")
+print(f"Hello {input_name}, nice to meet you!")
+print("How can I assist you today?")
+user_query = input("Type your query here: ")
+print(f"You asked: {user_query}")
+print("Thank you for reaching out. I will do my best to assist you!")
